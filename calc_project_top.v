3be68dba794fa94ad42c1f69edcdb3d0534d3e06
Tobias Lee <tobiasle@usc.edu>
2012-12-04 22:50:41 -0800

    SUP IT WORKS NOW

--- calc_project_top.v
+++ calc_project_top.v
@@ -269,7 +269,7 @@ module calc_project_top(ClkPort,
 	assign {data1, data2, data3, data4, data5, data6, data7, data8,
 				   data9, data10, data11, data12, data13, data14, data15, data16,
 					data17, data18, data19, data20, data21, data22, data23, data24,
-					data25, data26, data27, data28, data29, data30, data31, data32}  = (textToLCD);
+					data25, data26, data27, data28, data29, data30, data31, data32}  = {textToLCD};
 				
 	// Instantiate the Unit Under Test (UUT)
 	lcd_core uut (
